27c3558f9f74acba239aad5ecf3efb1d485888ec
Kayode Matthew <matthewkayode116@gmail.com>
2022-03-26 14:09:33 +0100

    change folder structere

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import { Signin, Signup } from './auth/views';
 
 function App() {
   return (
-    <div>
-      <Signup />
-      <Signin />
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/signup">Signup Page</Link>
+        </li>
+        <li>
+          <Link to="/signin">Signin Page</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </Router>
   );
 }
 
